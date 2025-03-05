e9540a2327dea7474d5ab2844eb40a2f207a72c0
sriraghulM <sriraghul0777@gmail.com>
2025-03-05 17:40:53 +0530

    Initial commit4

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./Studies.css";
-import Login from "./Login1";
+import Login from "./Login2";
+import Catalogue from "./Catalogue";
 
 export default function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Login />} />
+        <Route path="/catalogue" element={<Catalogue />} />
       </Routes>
     </Router>
   );
 }
-//app//
